7a65bb96fee8a764ce782681ee3251ec63188cc6
Aliaksandr Vorakh <aliaksandrvorakh@gmail.com>
2024-11-24 19:51:13 +0100

    refactor: improve code coverage

--- demo-web-app/src/main/java/dev/avorakh/demo/application/DemoWebApplication.java
+++ demo-web-app/src/main/java/dev/avorakh/demo/application/DemoWebApplication.java
@@ -1,10 +1,13 @@
 package dev.avorakh.demo.application;
 
+import lombok.Generated;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoWebApplication {
+
+    @Generated
     public static void main(String[] args) {
         SpringApplication.run(DemoWebApplication.class, args);
     }
